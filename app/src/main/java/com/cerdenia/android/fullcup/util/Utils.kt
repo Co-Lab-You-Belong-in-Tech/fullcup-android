27ff59987ee34e03efe30a72759d9aff21e02438
Joshua Cerdenia <joshcerdenia@gmail.com>
2021-05-24 10:04:50 +0800

    Change status bar color

---
+++ app/src/main/java/com/cerdenia/android/fullcup/util/Utils.kt
@@ -0,0 +1,12 @@
+package com.cerdenia.android.fullcup.util
+
+import android.app.Activity
+import android.content.res.Configuration
+import android.graphics.Color
+import android.os.Build
+import android.view.View
+import android.view.WindowManager
+
+object Utils {
+
+}
